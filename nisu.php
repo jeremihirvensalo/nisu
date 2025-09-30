5443a804ec03d77db860f453a91d6019602bf426
jeremihirvensalo <jeremi.hirvensalo@gmail.com>
2025-09-30 16:51:45 +0300

    Started using PSR-4 autoloading

--- nisu.php
+++ nisu.php
@@ -11,8 +11,7 @@ defined("ABSPATH") || exit;
 define("NISU_PLUGIN_PATH", plugin_dir_path(__FILE__));
 define("NISU_PLUGIN_URL", plugin_dir_url(__FILE__));
 
-foreach(glob(NISU_PLUGIN_PATH . "inc/{**/*,*}.php", GLOB_BRACE) as $filename) {
-  require_once $filename;
-} 
+// Autoload classes
+require_once NISU_PLUGIN_PATH . "vendor/autoload.php";
 
-add_filter("wp_generate_attachment_metadata", "\NISU\Main::rename_image_sizes", 10, 1);
+add_filter("wp_generate_attachment_metadata", [NISU\Main::class, "rename_image_sizes"], 10, 1);
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '35429f0cdf49a20d97fcb33040f17a92baa2ae90',
+        'name' => 'jeremihirvensalo/nisu',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jeremihirvensalo/nisu' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '35429f0cdf49a20d97fcb33040f17a92baa2ae90',
+            'dev_requirement' => false,
+        ),
+    ),
+);
